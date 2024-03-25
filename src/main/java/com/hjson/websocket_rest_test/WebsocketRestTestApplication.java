b4dbf0e0ee1a59397922902389f1f11a723aef9c
Huijun Son <h.json248@gmail.com>
2024-03-25 17:59:42 +0900

    Initial commit

---
+++ src/main/java/com/hjson/websocket_rest_test/WebsocketRestTestApplication.java
@@ -0,0 +1,13 @@
+package com.hjson.websocket_rest_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketRestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketRestTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjson/websocket_rest_test/WebsocketRestTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjson.websocket_rest_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketRestTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
